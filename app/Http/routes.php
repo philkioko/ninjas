165f176affa5de86a9d2fc028b00ffaa70d034ec
Phil <kiokophil@gmail.com>
2016-07-11 10:48:24 +0300

    new commit

---
+++ app/Http/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->get('hello', '\App\Http\Controllers\HomeController@index');
+    $api->post('authenticate', '\App\Http\Controllers\Auth\AuthController@authenticate');
+});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
